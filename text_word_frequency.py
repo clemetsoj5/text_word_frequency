571f5024420089529282cf29c07bb32f989ad001
Jake Clemetson <jclemet531@gmail.com>
2023-05-08 07:49:04 -0400

    adding ability to find the top 10 most frequently used words

--- text_word_frequency.py
+++ text_word_frequency.py
@@ -1,6 +1,7 @@
 
 from tkinter.filedialog import *
 import re
+from itertools import islice
 
 def open_and_read_file():
     # Have user select file to open
@@ -25,8 +26,21 @@ def split_text_into_words(data):
             word_counts[word] = 1
     return(word_counts)
 
+def find_top_ten(word_counts):
+    # Sorting word list in descending order
+    sorted_dict = {k: v for k, v in sorted(word_counts.items(), key=lambda item: item[1], reverse=True)}
+
+    # Grabbing and retuning the top 10 most frequently used words. If less than 10 words, then return the entire dict
+    if(len(sorted_dict) > 10):
+        top_ten = dict(islice(sorted_dict.items(), 10))
+    else:
+        top_ten = sorted_dict
+    return(top_ten)
+
 # Open file from user input and read text file
 data = open_and_read_file()
 # Grab all the word counts
 word_counts = split_text_into_words(data)
-print(word_counts)
+# Find the top 10 items
+top_word_counts = find_top_ten(word_counts)
+print(top_word_counts)
